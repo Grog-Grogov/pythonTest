8452662cefe3a466f08fc2475a09e3ffe88d4397
Grog <groggrogov@yandex.ru>
2022-07-31 12:07:58 +0300

    les_31_07_click_button

---
+++ main.py
@@ -0,0 +1,7 @@
+import my_module
+from my_module import say_hellow, name_2
+my_module.say_hi()
+print(my_module)
+
+say_hellow()
+print(name_2)
---
+++ my_module.py
@@ -0,0 +1,11 @@
+def say_hi():
+    print('hi')
+
+name = 'Fox'
+say_hi()
+
+def say_hellow():
+    print('Hi do you feel?')
+
+name_2 = 'world'
+say_hellow()
